9b5d1ffda82bdcc72b591501de90e328d4c9ee60
shliamin <efimshliamin@gmail.com>
2023-09-03 20:49:49 +0200

    Configure JPA and Initialized Data

--- currency-exchange-service/src/main/java/songsMS/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/songsMS/currencyexchangeservice/CurrencyExchange.java
@@ -2,11 +2,27 @@ package songsMS.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+
+@Entity
 public class CurrencyExchange {
+	
+	@Id
 	private Long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	
+	@Column(name="currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
+	private String environment;
+	
+	
 	
 	public CurrencyExchange() {
 		
@@ -42,6 +58,14 @@ public class CurrencyExchange {
 	}
 	public void setConversionMultiple(BigDecimal conversionMultiple) {
 		this.conversionMultiple = conversionMultiple;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
 	}
 	
 	
