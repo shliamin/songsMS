355475738ebae23611819922f69cdc66bb3cff8d
shliamin <efimshliamin@gmail.com>
2023-09-19 09:55:10 +0200

    Connecting Currency Conversion Microservices& API Gateway with Zipkin

--- currency-exchange-service/src/main/java/songsMS/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/songsMS/currencyexchangeservice/CurrencyExchangeController.java
@@ -5,10 +5,16 @@ package songsMS.currencyexchangeservice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
 
 @RestController
 public class CurrencyExchangeController {
