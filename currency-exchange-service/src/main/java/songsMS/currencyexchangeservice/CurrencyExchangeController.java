973366e63ece9e22fdafd2cc3d31737fa453a40a
shliamin <efimshliamin@gmail.com>
2023-09-19 08:33:08 +0200

    Connecting Currency Exhcnage Microservice with Zipkin

--- currency-exchange-service/src/main/java/songsMS/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/songsMS/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package songsMS.currencyexchangeservice;
 
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	
 	@Autowired
 	private CurrencyExchangeRepository repository;
@@ -21,6 +25,9 @@ public class CurrencyExchangeController {
 	public CurrencyExchange retrieveExhcnageValue(
 			@PathVariable String from,
 			@PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
+		
 		CurrencyExchange currencyExchange 
 					= repository.findByFromAndTo(from, to); 
 		if(currencyExchange == null) {
