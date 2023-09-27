dd21255a4cdc91b760d8a97ecf5cdbbee9855669
shliamin <efimshliamin@gmail.com>
2023-09-27 15:46:16 +0200

    creating songsLists microservice

---
+++ songsLists/src/main/java/songsMS/songsLists/SongsListsApplication.java
@@ -0,0 +1,13 @@
+package songsMS.songsLists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongsListsApplication.class, args);
+	}
+
+}
---
+++ songsLists/src/test/java/songsMS/songsLists/SongsListsApplicationTests.java
@@ -0,0 +1,13 @@
+package songsMS.songsLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
